70f3e9a4d1c09b0a6f28974acdd4a76801abe7e3
Linh Ta <linhtt@tiin.vn>
2020-05-15 22:21:10 +0700

    Initial Commit

---
+++ JellyGif/JellyGif.h
@@ -0,0 +1,19 @@
+//
+//  JellyGif.h
+//  JellyGif
+//
+//  Created by Linh Ta on 5/15/20.
+//  Copyright © 2020 Linh Ta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JellyGif.
+FOUNDATION_EXPORT double JellyGifVersionNumber;
+
+//! Project version string for JellyGif.
+FOUNDATION_EXPORT const unsigned char JellyGifVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JellyGif/PublicHeader.h>
+
+
